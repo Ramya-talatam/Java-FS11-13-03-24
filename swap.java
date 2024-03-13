cbb91a93c07178e595bb1c2a1041fa47ecc48a91
Ramya-talatam <108510824+Ramya-talatam@users.noreply.github.com>
2024-03-13 16:14:11 +0530

    Finished assignment

--- swap.java
+++ swap.java
@@ -1,6 +1,6 @@
 /* 2. write a java program to swap two numbers without using a third variable and (+ and -) operators? int a = 200, b = 300; */
 
-
+import java.util.*;
 public class swap
 {
   public static void main (String[]args)
@@ -10,8 +10,16 @@ public class swap
 	  a = a ^ b;
 	  b = a ^ b;
 	  a = a ^ b;
+	  
 	//xorring a number with itself is 0 so I utilised this logic
-	  System.out.println ("After swapping, a = " + a + " b = " + b);
-
+	  System.out.println ("After swapping using xor, a = " + a + " b = " + b);
+	  
+	 //re-declaring original values
+	 a=200;b=300;
+	 List l = new ArrayList<Integer>(2);
+	 l.add(a);
+	 l.add(b);
+	 Collections.swap(l,0,1);
+	 System.out.println ("After swapping using inbuit swap, "+l);
   }
 }
